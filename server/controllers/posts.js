116c5dec00f28b7f0794b3b28e76cbe8f646c518
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-06-07 13:58:11 -0500

    Like count works

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -46,3 +46,19 @@ export const deletePost = async (req, res) => {
 	await PostMessage.findByIdAndRemove(id);
 	res.status(200).json({ message: 'Post deleted' });
 };
+
+export const likePost = async (req, res) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id))
+		return res.status(404).send('No post with that id exists');
+
+	const post = await PostMessage.findById(id);
+	const updatedPost = await PostMessage.findByIdAndUpdate(
+		id,
+		{ likeCount: post.likeCount + 1 },
+		{ new: true }
+	);
+
+	res.json(updatedPost);
+};
